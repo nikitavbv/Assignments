5d9d1035b0787aff32307f7d463961ae7aebb6dd
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-15 01:25:10 +0300

    feat(MyHashTable): add remove method

--- src/main/java/com/nikitavbv/assignments/algorithms/lab2/MyHashTable.java
+++ src/main/java/com/nikitavbv/assignments/algorithms/lab2/MyHashTable.java
@@ -1,5 +1,6 @@
 package com.nikitavbv.assignments.algorithms.lab2;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class MyHashTable<K, V> {
@@ -12,6 +13,7 @@ public class MyHashTable<K, V> {
 
   private K[] keyArr;
   private V[] valArr;
+  private boolean[] removed;
 
   private int size = 0;
   private boolean resizable;
@@ -22,6 +24,8 @@ public class MyHashTable<K, V> {
     this.keyArr = (K[]) new Object[capacity];
     // noinspection unchecked
     this.valArr = (V[]) new Object[capacity];
+    this.removed = new boolean[capacity];
+    Arrays.fill(this.removed, false);
   }
 
   MyHashTable(boolean resizable) {
@@ -39,12 +43,13 @@ public class MyHashTable<K, V> {
     if (size == keyArr.length) throw new MyHashTableFullException();
 
     int pos = Math.abs(key.hashCode()) % keyArr.length;
-    while (keyArr[pos] != null) {
+    while (keyArr[pos] != null && !this.removed[pos]) {
       pos = (pos + 1) % keyArr.length;
     }
 
     keyArr[pos] = key;
     valArr[pos] = value;
+    removed[pos] = false;
 
     size++;
   }
@@ -52,19 +57,20 @@ public class MyHashTable<K, V> {
   private void resize() {
     MyHashTable<K, V> temp = new MyHashTable<>();
     for (int i = 0; i < keyArr.length; i++) {
-      if (keyArr[i] == null) {
+      if (keyArr[i] == null || this.removed[i]) {
         continue;
       }
       temp.put(keyArr[i], valArr[i]);
     }
     this.keyArr = temp.keyArr;
     this.valArr = temp.valArr;
+    this.removed = temp.removed;
   }
 
   public Optional<V> get(K key) {
     int pos = Math.abs(key.hashCode()) % keyArr.length;
     while (keyArr[pos] != null) {
-      if (keyArr[pos] == key) {
+      if (keyArr[pos] == key && !this.removed[pos]) {
         return Optional.of(valArr[pos]);
       }
       pos = (pos + 1) % keyArr.length;
@@ -81,12 +87,25 @@ public class MyHashTable<K, V> {
     return get(key).orElse(defaultValue);
   }
 
+  public void remove(K key) {
+    int pos = Math.abs(key.hashCode()) % keyArr.length;
+    while (keyArr[pos] != null) {
+      if (keyArr[pos] == key && !this.removed[pos]) {
+        this.removed[pos] = true;
+        return;
+      }
+      pos = (pos + 1) % keyArr.length;
+    }
+  }
+
   public void clear() {
     size = 0;
     // noinspection unchecked
     this.keyArr = (K[]) new Object[this.keyArr.length];
     // noinspection unchecked
     this.valArr = (V[]) new Object[this.valArr.length];
+    this.removed = new boolean[this.removed.length];
+    Arrays.fill(this.removed, false);
   }
 
   public int getSize() {
--- src/test/java/com/nikitavbv/assignments/algorithms/lab2/MyHashTableTests.java
+++ src/test/java/com/nikitavbv/assignments/algorithms/lab2/MyHashTableTests.java
@@ -1,6 +1,7 @@
 package com.nikitavbv.assignments.algorithms.lab2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -34,6 +35,20 @@ public class MyHashTableTests {
     assertEquals(5, myHashTable.get("hello").get().intValue());
     assertEquals(5, myHashTable.get("world").get().intValue());
     assertEquals(10, myHashTable.get("hello world").get().intValue());
+  }
+
+  @Test
+  public void testRemove() {
+    MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+    myHashTable.put("foobar", 42);
+    myHashTable.put("Aa", 1);
+    myHashTable.put("BB", 2); // cause collision
+    myHashTable.remove("BB");
+    assertFalse(myHashTable.get("BB").isPresent());
+    myHashTable.remove("Aa");
+    assertFalse(myHashTable.get("Aa").isPresent());
+    myHashTable.remove("foobar");
+    assertFalse(myHashTable.get("foobar").isPresent());
   }
 
 }
